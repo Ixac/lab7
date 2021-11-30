8272326ac77bcc1372935f536201e66dc9ec704a
Ixac <ixacarrrows@gmail.com>
2021-11-30 16:48:28 +0700

    lab7 3

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,15 +1,24 @@
 #include<iostream>
-
+#include <cmath>
 using namespace std;
+
+int adiff(int a,int b){
+int c;
+int d;
+
+ c=sqrt(pow(a-b,2));
+ if(c>180){
+ d= sqrt(pow(360-c,2));
+ return d%360;
+}
+else{return c%360;}
+
+}
+
+
+
 
 
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+cout << adiff( 710,-180);
 }
